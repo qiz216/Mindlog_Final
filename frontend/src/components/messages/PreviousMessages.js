a23d09213df6d74c51cd51fda0d31bf0b64cb19f
JBotwina <jimbotwina16@gmail.com>
2020-07-24 18:14:48 -0400

    working on setting state

--- frontend/src/components/messages/PreviousMessages.js
+++ frontend/src/components/messages/PreviousMessages.js
@@ -1,50 +1,101 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, Badge } from "antd";
 import { func } from "prop-types";
 import moment from "moment";
+import axios from "axios";
+import { tokenConfig } from "../../actions/auth";
+import { connect } from "react-redux";
+import store from "../../store";
 
-function getListData(value) {
-  let listData;
+function PreviousMessages() {
+  const [month, setMonth] = useState(moment().month() + 1);
+  const [numRefThisMonth, setNumRefThisMonth] = useState(0);
+  const [listOfRefsByDate, setListOfRefsByDate] = useState(new Map());
 
-  switch (value.date()) {
-    case 8:
-      listData = [{ type: "success", content: "Reflections" }];
-      break;
-    case 10:
-      listData = [{ type: "success", content: "Reflections " }];
-      break;
-    case 15:
-      listData = [{ type: "success", content: "Reflections" }];
-      break;
-    default:
+  useEffect(() => {
+    console.log(`Month is ${month}`);
+    axios
+      .get(`/api/messages?month=${month}`, tokenConfig(store.getState))
+      .then((res) => {
+        setNumRefThisMonth(res.data.length);
+
+        console.log(res);
+
+        res.data.map((day) => {
+          const dayNum = day.created_at.substring(8, 10);
+          console.log(dayNum);
+          if (listOfRefsByDate.has(dayNum)) {
+            //setListOfRefsByDate(...listOfRefsByDate, dayNum : listOfRefsByDate.get(dayNum) + 1 );
+            listOfRefsByDate.set(dayNum, listOfRefsByDate.get(dayNum) + 1);
+          } else {
+            listOfRefsByDate.set(dayNum, 1);
+          }
+        });
+        console.log(listOfRefsByDate);
+      })
+      .catch((err) => console.log(err));
+  }, [month]);
+
+  function getListData(value) {
+    let listData;
+
+    switch (value.date()) {
+      case 11:
+        listData = [{ type: "success", content: "Reflections" }];
+        break;
+      case 20:
+        listData = [{ type: "success", content: "Reflections " }];
+        break;
+      case 22:
+        listData = [{ type: "success", content: "Reflections" }];
+        break;
+      default:
+    }
+    return listData || [];
   }
-  return listData || [];
-}
 
-function dateCellRender(value) {
-  const listData = getListData(value);
-  return (
-    <dl className="events">
-      {listData.map((item) => (
-        <dd key={item.content}>
-          <Badge status={item.type} text={item.content} />
-        </dd>
-      ))}
-    </dl>
-  );
-}
+  function dateCellRender(value) {
+    //const listData = getListData(value);
+    let listData = [];
+    if (listOfRefsByDate.has(15)) {
+      console.log("We got it");
+    }
+    console.log(`This is working ${listOfRefsByDate}`);
 
-export default function PreviousMessages() {
+    if (listOfRefsByDate.has(value.date().toString())) {
+      listData = [
+        {
+          type: "success",
+          content: `${listOfRefsByDate.get(value.date())} Reflections`,
+        },
+      ];
+    } else {
+      //console.log("Something is up");
+    }
+    return (
+      <dl className="events">
+        {listData.map((item) => (
+          <dd key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </dd>
+        ))}
+      </dl>
+    );
+  }
+
   //when a date is selected
   function onSelect(date) {
-    // console.log(date.date());
+    console.log(`The date is ${date.date()}`);
   }
   //this handles when we change the month
   function onPanelChange(date) {
+    setListOfRefsByDate(new Map());
+    setMonth(date.month() + 1);
     console.log(date.month());
   }
   return (
     <div>
+      <h3>You have {numRefThisMonth} reflections this month</h3>
       <Calendar
         dateCellRender={dateCellRender}
         onPanelChange={onPanelChange}
@@ -53,3 +104,9 @@ export default function PreviousMessages() {
     </div>
   );
 }
+
+const mapStateToProps = (state) => ({
+  auth: state.authReducer,
+});
+
+export default connect(mapStateToProps)(PreviousMessages);
